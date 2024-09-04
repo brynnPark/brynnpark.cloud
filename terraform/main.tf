e211ab6a7426c23f9998d45b61319bc07dada749
Brynn Park <pbh7080@gmail.com>
2024-09-05 04:12:34 +0900

    delete upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,11 +59,3 @@ resource "aws_s3_bucket_policy" "bucket-policy" {
   ]
 }
 
-resource "aws_s3_object" "upload-build-file" {
-    for_each        = fileset("../my_web_page/build/", "**")
-
-    bucket          = "${var.static_bucket_name}"
-    key             = each.value
-    source          = "../my_web_page/build/${each.value}"
-    etag            = filemd5("../my_web_page/build/${each.value}")
-}
