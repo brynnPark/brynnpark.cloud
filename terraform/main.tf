5fe63a004be98eabf61441e27c4f8bf6f13a44f7
Brynn Park <pbh7080@gmail.com>
2024-09-05 01:14:25 +0900

    build find test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,10 @@
 
 resource "aws_s3_object" "upload-build-file" {
-    for_each        = fileset("../my_web_page/public/", "**")
+    #for_each        = fileset("../my_web_page/build/", "**")
 
     bucket          = var.static_bucket_name
-    key             = each.value
-    source          = "../my_web_page/public/${each.value}"
-    etag            = filemd5("../my_web_page/public/${each.value}")
+    key             = "index.html"
+    source          = "../my_web_page/build/index.html"
+    #etag            = filemd5("../my_web_page/public/${each.value}")
     acl             = "public-read"
-}
-
-output "files_in_build_directory" {
-  value = fileset("../my_web_page/public/", "**")
 }
