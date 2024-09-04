37b78704830dbf8fb4e690ce146ee9cb60ca0ac6
Brynn Park <pbh7080@gmail.com>
2024-09-05 04:55:30 +0900

    ddd

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,17 @@ resource "aws_s3_object" "upload-build-file" {
     bucket          = "${var.static_bucket_name}"
     key             = each.value
     source          = "../my_web_page/build/${each.value}"
-    etag            = filemd5("../my_web_page/build/${each.value}")
+    # etag            = filemd5("../my_web_page/build/${each.value}")
 
-    content_type = lookup(local.content_type_map, split(".", "../my_web_page/build/${each.value}")[1], "text/html")
-
+    content_type = lookup({
+    "html" = "text/html",
+    "css"  = "text/css",
+    "js"   = "application/json",
+    "png"  = "image/png",
+    "jpg"  = "image/jpeg",
+    "jpeg" = "image/jpeg",
+    "gif"  = "image/gif",
+    "svg"  = "image/svg+xml",
+    "ico"  = "image/x-icon"
+  }, split(".", each.value)[length(split(".", each.value)) - 1], "application/octet-stream")
 }
