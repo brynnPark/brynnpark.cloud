19730af48113319e352fee8a963de1eb8389c1dd
Brynn Park <pbh7080@gmail.com>
2024-09-05 02:15:27 +0900

    remove build from .gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 
 resource "aws_s3_object" "upload-build-file" {
-    #for_each        = fileset("../my_web_page/build/", "*.html")
+    for_each        = fileset("../my_web_page/build/", "**/*.*")
 
     bucket          = var.static_bucket_name
-    key             = "package.json"
-    source          = "../my_web_page/package.json"
-    #etag            = filemd5("../my_web_page/build/${each.value}")
+    key             = each.value
+    source          = "../my_web_page/build/${each.value}"
+    etag            = filemd5("../my_web_page/build/${each.value}")
     acl             = "public-read"
 }
