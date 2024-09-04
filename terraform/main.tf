4a853e9ba2ccab5222488e14cb001e5ab97a347e
Brynn Park <pbh7080@gmail.com>
2024-09-05 04:07:19 +0900

    bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,7 +60,7 @@ resource "aws_s3_bucket_policy" "bucket-policy" {
 }
 
 resource "aws_s3_object" "upload-build-file" {
-    for_each        = fileset("../my_web_page/build/", "**/*.*")
+    for_each        = fileset("../my_web_page/build/", "**")
 
     bucket          = "${var.static_bucket_name}"
     key             = each.value
