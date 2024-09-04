d3f6c231846c7d8068867a47907bf3847bc63d14
Brynn Park <pbh7080@gmail.com>
2024-09-05 02:02:45 +0900

    change path

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,11 @@
 
 resource "aws_s3_object" "upload-build-file" {
-    for_each        = fileset("../my_web_page/build/static", "**")
+    for_each        = fileset("../my_web_page/build/", "*.html")
 
     bucket          = var.static_bucket_name
     key             = each.value
-    source          = "../my_web_page/build/static/${each.value}"
-    etag            = filemd5("../my_web_page/build/static/${each.value}")
+    content_type = "text/html"
+    source          = "../my_web_page/build/${each.value}"
+    etag            = filemd5("../my_web_page/build/${each.value}")
     acl             = "public-read"
 }
