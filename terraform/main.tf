5ca72e74bcbf16b3ee8843ce3448d947074425a8
Brynn Park <pbh7080@gmail.com>
2024-09-05 04:13:26 +0900

    uupload

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,8 @@
-
 resource "aws_s3_object" "upload-build-file" {
-    for_each        = fileset("../my_web_page/build/", "**/*.*")
+    for_each        = fileset("../my_web_page/build/", "**")
 
-    bucket          = var.static_bucket_name
+    bucket          = "${var.static_bucket_name}"
     key             = each.value
     source          = "../my_web_page/build/${each.value}"
     etag            = filemd5("../my_web_page/build/${each.value}")
-    acl             = "public-read"
 }
