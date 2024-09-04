64f6cd47a37addd5a91ca62613e517db2a1f99f5
Brynn Park <pbh7080@gmail.com>
2024-09-05 02:28:17 +0900

    Complete all

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,3 +58,13 @@ resource "aws_s3_bucket_policy" "bucket-policy" {
   aws_s3_bucket_public_access_block.my-web-page
   ]
 }
+
+resource "aws_s3_object" "upload-build-file" {
+    for_each        = fileset("../my_web_page/build/", "**/*.*")
+
+    bucket          = var.static_bucket_name
+    key             = each.value
+    source          = "../my_web_page/build/${each.value}"
+    etag            = filemd5("../my_web_page/build/${each.value}")
+    acl             = "public-read"
+}
