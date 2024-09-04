d4c112b0046cd1d713c4b293bbc2fb309ad3027b
Brynn Park <pbh7080@gmail.com>
2024-09-05 00:12:52 +0900

    Correct upload path

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,42 @@
+
 resource "aws_s3_object" "upload-build-file" {
-    for_each        = fileset("my_web_page/build/", "*")
+    for_each        = fileset("../my_web_page/build/", "*")
 
     bucket          = var.static_bucket_name
     key             = each.value
-    source          = "my_web_page/build/${each.value}"
-    etag            = filemd5("my_web_page/build/${each.value}")
+    source          = "../my_web_page/build/${each.value}"
+    etag            = filemd5("../my_web_page/build/${each.value}")
     acl             = "public-read"
 }
 
 resource "aws_s3_object" "upload-index-html" {
-    for_each        = fileset("my_web_page/public/", "*.html")
+    for_each        = fileset("../my_web_page/public/", "*.html")
 
     bucket          = var.static_bucket_name
     key             = each.value
-    source          = "my_web_page/public/${each.value}"
-    content_type    = "html"
-    etag            = filemd5("my_web_page/public/${each.value}")
+    source          = "../my_web_page/public/${each.value}"
+    content_type    = "text/html"
+    etag            = filemd5("../my_web_page/public/${each.value}")
+    acl             = "public-read"
+}
+
+resource "aws_s3_object" "upload-src-file" {
+    for_each        = fileset("../my_web_page/src/", "*")
+
+    bucket          = var.static_bucket_name
+    key             = each.value
+    source          = "../my_web_page/src/${each.value}"
+    etag            = filemd5("../my_web_page/src/${each.value}")
     acl             = "public-read"
 }
 
 resource "aws_s3_object" "object-upload-jpg" {
-    for_each        = fileset("my_web_page/public/", "*.jpg")
+    for_each        = fileset("../my_web_page/public/", "*.jpg")
+
     bucket          = var.static_bucket_name
     key             = each.value
     content_type    = "image/jpg"
-    source          = "my_web_page/public/${each.value}"
-    etag            = filemd5("my_web_page/public/${each.value}")
+    source          = "../my_web_page/public/${each.value}"
+    etag            = filemd5("../my_web_page/public/${each.value}")
     acl             = "public-read"
 }
