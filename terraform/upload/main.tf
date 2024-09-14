e6f27f2152555a542b3f26641ddf6e85088273c8
Brynn Park <pbh7080@gmail.com>
2024-09-14 22:37:51 +0900

    edit the upload path

--- terraform/upload/main.tf
+++ terraform/upload/main.tf
@@ -36,7 +36,7 @@
 resource "null_resource" "delete_all_objects" {
   # Use the local-exec provisioner to run the AWS CLI command
   provisioner "local-exec" {
-    command = "echo 'Deleting all objects in S3 bucket' && aws s3 rm s3://${var.static_bucket_name} --recursive"
+    command = "echo 'Deleting all objects in S3 bucket' && aws s3 rm s3://${var.static_bucket_name} --recursive && sleep 3"
   }
 
   # Add a trigger to ensure this command runs when you apply changes
@@ -47,12 +47,12 @@ resource "null_resource" "delete_all_objects" {
 
 resource "aws_s3_object" "upload-build-file" {
     depends_on = [null_resource.delete_all_objects]
-    for_each        = fileset("../my_web_page/build/", "**")
+    for_each        = fileset("../../my_web_page/build/", "**")
 
     bucket          = "${var.static_bucket_name}"
     key             = each.value
-    source          = "../my_web_page/build/${each.value}"
-    # etag            = filemd5("../my_web_page/build/${each.value}")
+    source          = "../../my_web_page/build/${each.value}"
+    # etag            = filemd5("../../my_web_page/build/${each.value}")
 
     content_type = lookup({
     "html" = "text/html",
