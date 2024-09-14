24a58f9ed8b88b36fcfc1e8731e38fc0b14dee69
Brynn Park <pbh7080@gmail.com>
2024-09-15 04:43:00 +0900

    certificate

--- terraform/deploy/main.tf
+++ terraform/deploy/main.tf
@@ -2,65 +2,65 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_s3_bucket" "my-web-page" {
-  bucket = "${var.static_bucket_name}"
-}
+# resource "aws_s3_bucket" "my-web-page" {
+#   bucket = "${var.static_bucket_name}"
+# }
 
-resource "aws_s3_bucket_ownership_controls" "my-web-page" {
-  bucket = "${var.static_bucket_name}"
-  rule {
-  object_ownership = "BucketOwnerPreferred"
-  }
-  depends_on = [aws_s3_bucket.my-web-page]
-}
+# resource "aws_s3_bucket_ownership_controls" "my-web-page" {
+#   bucket = "${var.static_bucket_name}"
+#   rule {
+#   object_ownership = "BucketOwnerPreferred"
+#   }
+#   depends_on = [aws_s3_bucket.my-web-page]
+# }
  
-resource "aws_s3_bucket_public_access_block" "my-web-page" {
-  bucket = "${var.static_bucket_name}"
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+# resource "aws_s3_bucket_public_access_block" "my-web-page" {
+#   bucket = "${var.static_bucket_name}"
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-resource "aws_s3_bucket_acl" "my-web-page" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.my-web-page,
-    aws_s3_bucket_public_access_block.my-web-page,
-  ]
+# resource "aws_s3_bucket_acl" "my-web-page" {
+#   depends_on = [
+#     aws_s3_bucket_ownership_controls.my-web-page,
+#     aws_s3_bucket_public_access_block.my-web-page,
+#   ]
 
-  bucket = "${var.static_bucket_name}"
-  acl    = "public-read"
-}
+#   bucket = "${var.static_bucket_name}"
+#   acl    = "public-read"
+# }
 
-resource "aws_s3_bucket_website_configuration" "website-config" {
-  bucket = "${var.static_bucket_name}"
+# resource "aws_s3_bucket_website_configuration" "website-config" {
+#   bucket = "${var.static_bucket_name}"
 
-  index_document {
-  suffix = "index.html"
-  }
+#   index_document {
+#   suffix = "index.html"
+#   }
 
-  error_document {
-  key = "index.html"
-  }
-}
+#   error_document {
+#   key = "index.html"
+#   }
+# }
 
-resource "aws_s3_bucket_policy" "bucket-policy" {
-  bucket = "${var.static_bucket_name}"
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [{
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": ["s3:GetObject","s3:PutObject", "S3:DeleteObject"]
-            "Resource": "arn:aws:s3:::brynnpark.cloud/*"
-        }]
-    })
+# resource "aws_s3_bucket_policy" "bucket-policy" {
+#   bucket = "${var.static_bucket_name}"
+#   policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [{
+#             "Sid": "PublicReadGetObject",
+#             "Effect": "Allow",
+#             "Principal": "*",
+#             "Action": ["s3:GetObject","s3:PutObject", "S3:DeleteObject"]
+#             "Resource": "arn:aws:s3:::brynnpark.cloud/*"
+#         }]
+#     })
 
-  depends_on = [
-  aws_s3_bucket_public_access_block.my-web-page
-  ]
-}
+#   depends_on = [
+#   aws_s3_bucket_public_access_block.my-web-page
+#   ]
+# }
 
 provider "aws" {
   alias  = "us_east_1"
@@ -163,7 +163,6 @@ resource "aws_cloudfront_distribution" "www_distribution" {
     acm_certificate_arn = "${aws_acm_certificate.certificate.arn}"
     ssl_support_method  = "sni-only"
   }
-  depends_on = [aws_acm_certificate.certificate ]
 }
 
 // This Route53 record will point at our CloudFront distribution.
