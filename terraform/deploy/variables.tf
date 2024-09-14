d26f76953b8b2243a67cbf4d4f7497bddc9a26cf
Brynn Park <pbh7080@gmail.com>
2024-09-14 23:14:27 +0900

    Add cloudfront dns

--- terraform/deploy/variables.tf
+++ terraform/deploy/variables.tf
@@ -4,3 +4,13 @@ variable "static_bucket_name" {
   description = "The name of S3 bucket for static website hosting"
   default = "brynnpark.cloud"
 }
+
+// Create a variable for our domain name because we'll be using it a lot.
+variable "www_domain_name" {
+  default = "www.brynnpark.cloud"
+}
+
+variable "root_domain_name" {
+  default = "brynnpark.cloud"
+}
+
