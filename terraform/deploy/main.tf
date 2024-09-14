cc704a92891ad91886fd4d5f0fa603df8c5768bd
Brynn Park <pbh7080@gmail.com>
2024-09-14 22:36:39 +0900

    policy upload

--- terraform/deploy/main.tf
+++ terraform/deploy/main.tf
@@ -47,7 +47,7 @@ resource "aws_s3_bucket_policy" "bucket-policy" {
             "Sid": "PublicReadGetObject",
             "Effect": "Allow",
             "Principal": "*",
-            "Action": ["s3:GetObject","s3:PutObject"]
+            "Action": ["s3:GetObject","s3:PutObject","s3:DeleteObject"]
             "Resource": "arn:aws:s3:::brynnpark.cloud/*"
         }]
     })
